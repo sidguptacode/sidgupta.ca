1ec6076f373f40f9ac05103af7894b788424541d
Sid Gupta <sid.gupta@mail.utoronto.ca>
2018-09-09 19:49:49 -0400

    Left side all good

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import roboto from '../robotonew.png';
+import roboto from '../images/robotonew.png';
+
+const borderStyles = {
+  borderColor: '#e8e8e8',
+  borderWidth: 1,
+  borderRadius: 0,
+  borderStyle: 'solid',
+  backgroundColor: 'white'
+};
 
 class HomePage extends Component {
   render() {
     return (
       <Grid container xs={12} sm={12} md={12} lg={12} xl={12}>
-      <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Oswald|PT+Sans" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto|Poppins|Lato|Montserrat|Oswald|PT+Sans" rel="stylesheet"/>
         <Header/>
+        <Grid container  xs={12} sm={6} md={6} lg={6} xl={6} style={{marginTop: 30}}>
+          <LeftInfo/>
+        </Grid>
       </Grid>
     );
   }
@@ -75,32 +86,74 @@ class Header extends Component {
   }
 }
 
-/*class LeftInfo extends Component {
+class LeftInfo extends Component {
   render() {
     return (
-      <Grid container xs={12} sm={12} md={12} lg={12} xl={12}>
-        <Grid item xs={1} sm={1} md={1} lg={1} xl={1}/>
-        <Grid container xs={10} sm={10} md={4} lg={4} xl={4}>
-          <Grid item  xs={12} sm={12} md={12} lg={12} xl={12}>
-            <Typography variant="display3">
+      <Grid container xs={12} sm={12} md={12} lg={12} xl={12} >
+        <Grid item xs={1} sm={2} md={2} lg={2} xl={2}/>
+        <Grid container xs={10} sm={9} md={9} lg={9} xl={9}>
+          <Grid item  xs={12} sm={12} md={12} lg={12} xl={12} style={{marginBottom: 20}}>
+            <Typography variant="body2">
               <div style={{fontSize: 14, color: "#6B756D", fontFamily: "PT Sans"}}>
                 Welcome to my web page!
                 <br/>
                 {"I'm a second year Computer Science student at the University of Toronto, and also co-founder of "}
-                <div style={{color: "#6069E6"}}>
+                <span style={{color: "#6069E6"}}>
                   {"Knowtworthy"}
-                </div>
+                </span>
               </div>
             </Typography>
           </Grid>
+          <Grid item  xs={12} sm={12} md={12} lg={12} xl={12}  style={{marginBottom: 15}}>
+            <img src={roboto} style={{width: 236, height: 225, ...borderStyles}} />
+          </Grid>
           <Grid item  xs={12} sm={12} md={12} lg={12} xl={12}>
-            <img src={roboto} style={{width: 405, height: 385}} />
+            <Typography variant="body2">
+              <div style={{fontSize: 14, color: "#6B756D", fontFamily: "PT Sans"}}>
+                {"I hope to always be learning and building, in the many fields that interest me!"}
+              </div>
+            </Typography>
           </Grid>
         </Grid>
         <Grid item xs={1} sm={1} md={1} lg={1} xl={1}/>
       </Grid>
     );
   }
-}*/
+}
+
+class RightInfo extends Component {
+  render() {
+    return (
+      <Grid container xs={12} sm={12} md={12} lg={12} xl={12} >
+        <Grid item xs={1} sm={2} md={2} lg={2} xl={2}/>
+        <Grid container xs={10} sm={9} md={9} lg={9} xl={9}>
+          <Grid item  xs={12} sm={12} md={12} lg={12} xl={12} style={{marginBottom: 20}}>
+            <Typography variant="body2">
+              <div style={{fontSize: 14, color: "#6B756D", fontFamily: "PT Sans"}}>
+                Welcome to my web page!
+                <br/>
+                {"I'm a second year Computer Science student at the University of Toronto, and also co-founder of "}
+                <span style={{color: "#6069E6"}}>
+                  {"Knowtworthy"}
+                </span>
+              </div>
+            </Typography>
+          </Grid>
+          <Grid item  xs={12} sm={12} md={12} lg={12} xl={12}  style={{marginBottom: 15}}>
+            <img src={roboto} style={{width: 236, height: 225, ...borderStyles}} />
+          </Grid>
+          <Grid item  xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Typography variant="body2">
+              <div style={{fontSize: 14, color: "#6B756D", fontFamily: "PT Sans"}}>
+                {"I hope to always be learning and building, in the many fields that interest me!"}
+              </div>
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={1} sm={1} md={1} lg={1} xl={1}/>
+      </Grid>
+    );
+  }
+}
 
 export default HomePage;
