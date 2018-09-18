41e90acab9949e50ca68972c1b575fe8eb1b7fe5
Sid Gupta <sid.gupta@mail.utoronto.ca>
2018-09-17 20:26:07 -0400

    Languages works

--- src/Components/Projectspage.js
+++ src/Components/Projectspage.js
@@ -55,7 +55,6 @@ class ProjectsPage extends Component {
             <Grid item xs={1} sm={1} md={1} lg={1} xl={1}/>
             <Grid container xs={10} sm={10} md={10} lg={10} xl={10} >
               <LanguagesBox selectedTools={this.state.selectedTools} addSelectedTool={this.addSelectedTool} removeSelectedTool={this.removeSelectedTool}/>
-              <SkillsSection/>
             </Grid>
           </MuiThemeProvider>
         </Grid>
@@ -84,69 +83,19 @@ class LanguagesBox extends Component {
         </Grid>
         <Grid container xs={10} sm={10} md={10} lg={10} xl={10} style={{marginTop: 23}}>
           <Grid item xs={1} sm={1} md={1} lg={2} xl={1} >
-            <Button variant={this.props.selectedTools.indexOf("JavaScript") != -1 ? "contained" : "outlined"} color="primary"
-                    style={{width: 100, height: 10, fontSize: 12}} labelStyle={{height: 20}} size="small" mini
-                    onClick={() => {
-                      if(this.props.selectedTools.indexOf("JavaScript") == -1){
-                        this.props.addSelectedTool("JavaScript");
-                      }else{
-                        this.props.removeSelectedTool("JavaScript");
-                      }
-                    }}>
-              JavaScript
-            </Button>
+            <SkillButton skill={"JavaScript"} selectedTools={this.props.selectedTools} addSelectedTool={this.props.addSelectedTool}  removeSelectedTool={this.props.removeSelectedTool} />
           </Grid>
           <Grid item xs={1} sm={1} md={1} lg={2} xl={1} >
-            <Button variant={this.props.selectedTools.indexOf("Java") != -1 ? "contained" : "outlined"} color="primary"
-                    style={{width: 100, height: 10, fontSize: 12}} labelStyle={{height: 20}} size="small" mini
-                    onClick={() => {
-                      if(this.props.selectedTools.indexOf("Java") == -1){
-                        this.props.addSelectedTool("Java");
-                      }else{
-                        this.props.removeSelectedTool("Java");
-                      }
-                    }}>
-              Java
-            </Button>
+            <SkillButton skill={"Java"} selectedTools={this.props.selectedTools} addSelectedTool={this.props.addSelectedTool} removeSelectedTool={this.props.removeSelectedTool} />
           </Grid>
           <Grid item xs={1} sm={1} md={1} lg={2} xl={1} >
-            <Button variant={this.props.selectedTools.indexOf("Python") != -1 ? "contained" : "outlined"} color="primary"
-                    style={{width: 100, height: 10, fontSize: 12}} labelStyle={{height: 20}} size="small" mini
-                    onClick={() => {
-                      if(this.props.selectedTools.indexOf("Python") == -1){
-                        this.props.addSelectedTool("Python");
-                      }else{
-                        this.props.removeSelectedTool("Python");
-                      }
-                    }}>
-              Python
-            </Button>
+            <SkillButton skill={"Python"} selectedTools={this.props.selectedTools} addSelectedTool={this.props.addSelectedTool} removeSelectedTool={this.props.removeSelectedTool} />
           </Grid>
           <Grid item xs={1} sm={1} md={1} lg={2} xl={1} >
-            <Button variant={this.props.selectedTools.indexOf("HTML") != -1 ? "contained" : "outlined"} color="primary"
-                    style={{width: 100, height: 10, fontSize: 12}} labelStyle={{height: 20}} size="small" mini
-                    onClick={() => {
-                      if(this.props.selectedTools.indexOf("HTML") == -1){
-                        this.props.addSelectedTool("HTML");
-                      }else{
-                        this.props.removeSelectedTool("HTML");
-                      }
-                    }}>
-              HTML
-            </Button>
+            <SkillButton skill={"HTML"} selectedTools={this.props.selectedTools} addSelectedTool={this.props.addSelectedTool} removeSelectedTool={this.props.removeSelectedTool} />
           </Grid>
           <Grid item xs={1} sm={1} md={1} lg={2} xl={1} >
-            <Button variant={this.props.selectedTools.indexOf("CSS") != -1 ? "contained" : "outlined"} color="primary"
-                    style={{width: 100, height: 10, fontSize: 12}} labelStyle={{height: 20}} size="small" mini
-                    onClick={() => {
-                      if(this.props.selectedTools.indexOf("CSS") == -1){
-                        this.props.addSelectedTool("CSS");
-                      }else{
-                        this.props.removeSelectedTool("CSS");
-                      }
-                    }}>
-              CSS
-            </Button>
+            <SkillButton skill={"CSS"} selectedTools={this.props.selectedTools} addSelectedTool={this.props.addSelectedTool} removeSelectedTool={this.props.removeSelectedTool} />
           </Grid>
           <Grid item lg={12}>
           </Grid>
@@ -157,72 +106,62 @@ class LanguagesBox extends Component {
   }
 }
 
-class Button extends Component {
+class SkillButton extends Component {
   constructor(props){
     super(props);
+  }
+
+  isSelected = () => {
+    return this.props.selectedTools.indexOf(this.props.skill) != -1;
   }
 
   render(){
     return(
-
+      <Button variant={this.isSelected() ? "contained" : "outlined"} color="primary"
+              style={{width: 100, height: 10, fontSize: 12}} labelStyle={{height: 20}} size="small" mini
+              onClick={() => {
+                if(this.isSelected()){
+                  this.props.removeSelectedTool(this.props.skill);
+                }else{
+                  this.props.addSelectedTool(this.props.skill);
+                }
+              }}>
+        {this.props.skill}
+      </Button>
     );
   }
-
 }
 
-
-
-
-class SkillsSection extends Component {
+class Skills extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-    }
   }
 
   render() {
+    if(this.props.selectedTools == null){
+      return <div/>
+    }
     return (
       <Grid container xs={12} sm={12} md={12} lg={12} xl={12}>
-
         <Grid container xs={12} sm={12} md={4} lg={4} xl={4} style={{textAlign: "center"}}>
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <Typography variant="display3">
-              <div style={{fontSize: 18, color: "#232B98", fontFamily: "PT Sans"}}>
-                Front-end design
-              </div>
-            </Typography>
-          </Grid>
-          <Grid container xs={12} sm={12} md={12} lg={12} xl={12}>
-            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}/>
-            <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
-              <Divider width="100%"/>
-            </Grid>
-            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}/>
-          </Grid>
+          <SkillSection name={"Front-end design"}  selectedTools={this.props.selectedTools} removeSelectedTool={this.props.removeSelectedTool}
+                        tools={['React', 'Redux', 'Material-ui', 'Bootstrap']}/>
         </Grid>
         <Grid container xs={12} sm={12} md={4} lg={4} xl={4} style={{textAlign: "center"}}>
-          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <Typography variant="display3">
-              <div style={{fontSize: 18, color: "#232B98", fontFamily: "PT Sans"}}>
-                Front-end design
-              </div>
-            </Typography>
-          </Grid>
-          <Grid container xs={12} sm={12} md={12} lg={12} xl={12}>
-            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}/>
-            <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
-              <Divider width="100%"/>
-            </Grid>
-            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}/>
-          </Grid>
+          <SkillSection name={"Back-end development"}  selectedTools={this.props.selectedTools} removeSelectedTool={this.props.removeSelectedTool}
+                        tools={['Node', 'Websocket', 'ShareDB', 'Express', "Socket.io", "Mongodb"]} />
+        </Grid>
+        <Grid container xs={12} sm={12} md={4} lg={4} xl={4} style={{textAlign: "center"}}>
+          <SkillSection name={"Tools and App development"}  selectedTools={this.props.selectedTools} removeSelectedTool={this.props.removeSelectedTool}
+                        tools={['Git', 'LibGDX', 'Flask']} />
         </Grid>
       </Grid>
     );
   }
 }
 
-class Skill extends Component{
+class SkillSection extends Component{
 
   constructor(props){
     super(props);
@@ -230,7 +169,7 @@ class Skill extends Component{
 
   render(){
     return(
-      <Grid container xs={12} sm={12} md={4} lg={4} xl={4} style={{textAlign: "center"}}>
+      <Grid container  xs={12} sm={12} md={4} lg={12} xl={12} style={{textAlign: "center"}}>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}  >
           <Typography variant="display3">
             <div style={{fontSize: 18, color: "#232B98", fontFamily: "PT Sans"}}>
@@ -248,8 +187,13 @@ class Skill extends Component{
         <Grid container xs={12} sm={12} md={12} lg={12} xl={12}>
         <Grid item xs={1} sm={1} md={1} lg={1} xl={1}/>
         <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
-          {this.props.tools.map((tool) =>
-
+          {this.props.tools.slice(0, Math.floor(this.props.tools.length / 2)).map((tool) =>
+            <SkillButton skill={tool} addSelectedTool={this.props.addSelectedTool} removeSelectedTool={this.props.removeSelectedTool} />
+          )}
+        </Grid>
+        <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
+          {this.props.tools.slice(4, this.props.tools.length).map((tool) =>
+            <SkillButton skill={tool} addSelectedTool={this.props.addSelectedTool} removeSelectedTool={this.props.removeSelectedTool} />
           )}
         </Grid>
         <Grid item xs={1} sm={1} md={1} lg={1} xl={1}/>
