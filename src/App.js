ea2769ec4c76f41f857405eaab9d7c4896a6bd52
Sid Gupta <sid.gupta@mail.utoronto.ca>
2021-09-03 15:30:13 -0400

    Beginning refactoring from MUI

--- src/App.js
+++ src/App.js
@@ -1,57 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import HomePage from './Components/Homepage'
-import SkillsPage from './Components/SkillsPage'
-import ProjectsPage from './Components/ProjectsPage'
-import EducationPage from './Components/Education'
+import React from 'react';
+import HomePage from './Components/Homepage/Homepage'
 import { BrowserRouter } from 'react-router-dom';
 import { Switch, Route, Router, Redirect } from 'react-router-dom'
-import Button from  '@material-ui/core/Button';
-import scrollToComponent from 'react-scroll-to-component';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
+function App () {
+  return (
+    <BrowserRouter>
+      <Switch>
         <main>
-          <Switch>
-            <Route exact path='/' component={Main}/>
-            />
-          </Switch>
+          <Route exact path='/' component={HomePage}/>
         </main>
-      </BrowserRouter>
-    );
-  }
-}
-
-class Main extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      skillsRef: null,
-      coursesRef: null
-    }
-  }
-
-  scrollDown = (ref) => {
-    scrollToComponent(ref, {
-      offset: 0,
-      align: 'top',
-      duration: 500
-    });
-  }
-
-  render() {
-    return(
-      <div>
-        <HomePage scrollDown={(refType) => {this.scrollDown(this.state[refType]);}}/>
-        {/* <SkillsPage setSkillsRef={(ref) => {this.setState({skillsRef: ref});}}/> */}
-        {/* <ProjectsPage /> */}
-        {/* <EducationPage setCoursesRef={(ref) => {this.setState({coursesRef: ref});}} /> */}
-      </div>
-    );
-  }
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
